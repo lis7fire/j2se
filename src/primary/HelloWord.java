2e64e0526fa6f7ecff976ac70bd550d7d31da387
lis7fire <root@gmail.com>
2018-05-29 11:27:00 +0800

    we

--- src/primary/HelloWord.java
+++ src/primary/HelloWord.java
@@ -1,6 +1,7 @@
 package primary;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 import primary.entity.Hero;
 import primary.entity.charactor.Support;
@@ -9,7 +10,7 @@ public class HelloWord {
 	Hero teemo = new Hero("teemo");
 
 	public int method1(final int j) {
-		// j = 5; //这个能否执行？
+		// j = 5; //杩欎釜鑳藉惁鎵ц锛�
 		return j;
 	}
 
@@ -23,9 +24,9 @@ public class HelloWord {
 		garen.huixue(xuepin);
 		System.out.println("" + garen.getHp());
 		garen.setHp(30);
-		System.out.println("掉血了：" + garen.getHp());
+		System.out.println("获取血量" + garen.getHp());
 		garen.revive(garen);
-		System.out.println("复活之后：" + garen.getHp());
+		System.out.println("新的血量" + garen.getHp());
 		int[] a = new int[] { 6, 2, 7, 3, 5 };
 		int[] b = new int[10];
 		int c = (int) (Math.random() * 100);
@@ -49,6 +50,27 @@ public class HelloWord {
 
 		System.out.println(hhh instanceof Support);
 		System.out.println(hhh.getClass());
+		byte cc = 'c';
+		Byte byte1 = cc;
+		String str = "das";
+		System.out.println(Float.parseFloat("3.14e2"));
+		System.out.println("valueof : " + String.valueOf(3.1e4f));
+		double e;
+		int n = Integer.MAX_VALUE;
+		e = Math.pow(1 + 1d/ n, n);
+		System.err.println(e);
+		System.out.println(Math.E);
+		Scanner input =new Scanner(System.in);
+		System.out.println("请输入整数：");
+		int in1=input.nextInt();
+//		System.out.println("请输入字串：");
+		input.nextLine();//占位换行符
+		String in2=input.nextLine();
+		System.out.println("字串： "+in2);
+		char ch='q';
+		Character.toString(ch);		
+		str.toCharArray();
+		
 	}
 
 	static float bmi(float high, float haven) {
